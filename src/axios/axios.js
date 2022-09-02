07ba740535e87a50bb03951e107869dce162db42
Aman Makhija <amanmakhija2205@gmail.com>
2022-09-02 14:34:56 +0530

    second commit

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const instance = axios.create({ baseURL: "https://soshial.herokuapp.com/api" });
+const instance = axios.create({ baseURL: "https://soshial.herokuapp.com" });
 
 export default instance;
